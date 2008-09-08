fbf2e52e07018cf87caaf0d921048a3bac9888ee
yar <yar@laptop>
2008-09-08 04:37:59 +0400

    non-default locale in the demo (falling back to default for missing translations)

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -1,8 +1,13 @@
 class ApplicationController < ActionController::Base
   helper :all
-  before_filter :set_locale
+  before_filter :choose_and_set_locale
   I18n.record_missing_keys = true # if you want to record missing translations
   
   protect_from_forgery # :secret => '573aac96d913ad4eb8f97fafe708b656'
   layout 'application'
+  
+  def choose_and_set_locale
+    set_locale('ru-RU')
+    # set_locale('en-US')
+  end
 end
